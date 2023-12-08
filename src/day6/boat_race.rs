07d6fde57ff603f5e5ba9f1ce2d4a8065bf8d159
Xavier Fostier <xavier.fostier@mailfence.com>
2023-12-08 17:35:52 +0100

    temp

--- src/day6/boat_race.rs
+++ src/day6/boat_race.rs
@@ -3,36 +3,19 @@
 
 use std::fs;
 use std::io::{BufReader, BufRead};
-use std::ops::Deref;
 use std::path::Path;
-use std::str;
 use std::collections::LinkedList;
-use crate::day6::race_utils::Race;
+use regex_lite::Regex;
 
 pub fn boat_race(){
     // Linked list of array of bytes representing characters
     let mut list: LinkedList<crate::day6::race_utils::Race> = LinkedList::new();
 
-    // Loading file
     let file_path = Path::new("src/day6/data.txt");
-    let file = match fs::File::open(file_path) {
-        Ok(file) => file,
-        Err(error) => panic!("Problem opening the file: {:?}", error),
-    };
-    let reader = BufReader::new(file);
-
-    for line in reader.lines() {
-        let line = match line {
-            Ok(line) => line,
-            Err(error) => panic!("Failure when reading line from buffer: {:?}", error),
-        };
+    while let a = list.
+    for iter in list {
         println!("{}", line);
-        let mut splited = line.split(" ");
-        for iter in splited.clone() {
-            println!("HELLO {}", iter);
-        }
-        let time:u32 = splited.nth(1).unwrap().replace(" ","").parse::<u32>().unwrap(); // Spaces cleaning - TODO: may panic 
-        let button:u32 = splited.nth(0).unwrap().replace(" ","").parse::<u32>().unwrap(); // Spaces cleaning - TODO: may panic 
+
         let race = Race {
             race_time: time,
             record_distance: button
@@ -40,7 +23,7 @@ pub fn boat_race(){
         list.push_back(
             race
         )
-    };
+    }
 
     let mut iter = list.iter();
     let mut sum = 0;
@@ -53,10 +36,42 @@ pub fn boat_race(){
     println!("DAY6 sum: {} \n", sum);
 }
 
-fn readFile(file_path: &Path)-> LinkedList<T> {
+// Loading file
+fn read_File(file_path: &Path)-> LinkedList<LinkedList<String>> {
+    let file = match fs::File::open(file_path) {
+        Ok(file) => file,
+        Err(error) => panic!("Problem opening the file: {:?}", error),
+    };
+    let reader = BufReader::new(file);
 
+    let mut list:LinkedList<LinkedList<String>> = LinkedList::new();
+    for line in reader.lines() {
+        let line = match line {
+            Ok(line) => line,
+            Err(error) => panic!("Failure when reading line from buffer: {:?}", error),
+        };
+        println!("{}", line);
+        list.push_back(tokenize(line, String::from(" "), true));
+    }
+    return list;
 }
 
-fn sanitize(){
+// Tokenize and cleaning spaces lines
+fn tokenize(text: String, separator: String, cleaning_reg: Regex) -> LinkedList<String> {
+    let mut list:LinkedList<String> = LinkedList::new();
+    let mut splited = text.split(separator.as_str());
+    for iter in splited {
 
+        if iter == " " || iter == "\t" {
+            continue;
+        }
+
+        if remove_duplicate {
+            let cleaned = cleaning_reg.replace_all();
+            list.push_back(iter.replace(" ","")); // Spaces cleaning - TODO: may panic 
+        } else {
+        list.push_back(iter.to_string());
+        }
+    }
+    return list;
 }
